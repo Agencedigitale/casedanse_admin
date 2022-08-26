bdc6645a9c32cf1306bdeb72cf952f95e649ae19
oraclerevolution <mac.n.drix@gmail.com>
2022-08-26 03:39:09 +0000

    ajout d'une route non protegée

--- config/shield.js
+++ config/shield.js
@@ -134,7 +134,7 @@ module.exports = {
   csrf: {
     enable: true,
     methods: ['POST', 'PUT', 'DELETE'],
-    filterUris: ['/loginUser','/createReservation/:id'],
+    filterUris: ['/loginUser','/createReservation/:id','/create_discipline'],
     cookieOptions: {
       httpOnly: false,
       sameSite: true,
