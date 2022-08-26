454fdfb61007b5c1d89995de3ce3f6f550f1cc7a
oraclerevolution <mac.n.drix@gmail.com>
2022-08-26 03:39:43 +0000

    creation du model et du controller Discipline

---
+++ app/Controllers/Http/DisciplineController.js
@@ -0,0 +1,100 @@
+'use strict'
+const Database = use('Database')
+const Discipline = use('App/Models/Discipline')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with disciplines
+ */
+class DisciplineController {
+  /**
+   * Show a list of all disciplines.
+   * GET disciplines
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+  }
+
+  /**
+   * Render a form to be used for creating a new discipline.
+   * GET disciplines/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new discipline.
+   * POST disciplines
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+    const discipline = await Discipline.create({
+      discpline: request.input('discipline')
+    })
+
+    await discipline.save()
+  }
+
+  /**
+   * Display a single discipline.
+   * GET disciplines/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing discipline.
+   * GET disciplines/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update discipline details.
+   * PUT or PATCH disciplines/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a discipline with id.
+   * DELETE disciplines/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = DisciplineController
